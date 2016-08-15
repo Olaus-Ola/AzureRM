87ba48692fbf515aaa5a1007b6b22820a8796465
Stuart Shay <sshay@yahoo.com>
2016-08-15 10:48:04 -0400

    Updated IIS DSC Script

--- win-node/dsc/setup-iis-web.ps1
+++ win-node/dsc/setup-iis-web.ps1
@@ -10,24 +10,53 @@ Configuration WebSite
 
     Node $NodeName { 
 
-	  WindowsFeature InstallWebServer 
+	  WindowsFeature IIS 
 	  { 
 	        Name = "Web-Server"
             Ensure = "Present"
       }
 
+      WindowsFeature IISManagement  
+      {  
+            Ensure          = "Present"
+            Name            = "Web-Mgmt-Console"
+            DependsOn       = "[WindowsFeature]IIS"
+      } 
+
+
+      xWebsite DefaultSite   
+      {  
+            Ensure          = "Present"
+            Name            = "Default Web Site"
+            State           = "Stopped"
+            PhysicalPath    = "C:\inetpub\wwwroot" 
+            DependsOn       = "[WindowsFeature]IIS"
+      } 
+
+
       xRemoteFile DotNetCoreWindowsHosting
       {  
-        Uri             = "https://go.microsoft.com/fwlink/?LinkId=817246"
-        DestinationPath = "C:\setup\DotNetCore.1.0.0-WindowsHosting.exe"
+            Uri             = "https://go.microsoft.com/fwlink/?LinkId=817246"
+            DestinationPath = "C:\setup\DotNetCore.1.0.0-WindowsHosting.exe"
       } 
 
 
       xRemoteFile DotNetCoreSDK
       {  
-        Uri             = "https://go.microsoft.com/fwlink/?LinkID=809122"
-        DestinationPath = "C:\setup\DotNetCore.1.0.0-SDK.Preview2-x64"
+            Uri             = "https://go.microsoft.com/fwlink/?LinkID=809122"
+            DestinationPath = "C:\setup\DotNetCore.1.0.0-SDK.Preview2-x64"
       } 
+
+
+      xWebAppPool WebAppAppPool   
+      {  
+            Ensure          = "Present"  
+            Name            = "web-app" 
+            State           = "Started"
+            managedRuntimeVersion = ""
+      } 
+
+
 
 
 
