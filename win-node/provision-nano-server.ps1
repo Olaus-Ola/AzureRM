2ba079f6f7fa1429286825298a4cb6aea129fd3a
Stuart Shay <sshay@yahoo.com>
2016-08-16 00:37:30 -0400

    Clean Up

--- win-node/provision-nano-server.ps1
+++ win-node/provision-nano-server.ps1
@@ -10,66 +10,6 @@ $SubNetIndex = 2
 
 $StorageAccountName = "azurestoragez1"
 
-
-#https://blogs.technet.microsoft.com/nanoserver/2016/05/27/nano-server-tp5-iaas-image-in-azure-updated
-Set-ExecutionPolicy -ExecutionPolicy Unrestricted -scope CurrentUser
-Import-Module .\modules\NanoServerAzureHelper.psm1 -Verbose 
-
-
-New-NanoServerAzureVM -Location $Location –VMName "nanoserver" -AdminUsername "Navigator" `
-                      -VaultName $VaultName -ResourceGroupName $ResourceGroupName -Verbose 
-
-
-Get-AzureRmPublicIpAddress -ResourceGroupName "AzureRM"
-
-
-# PowerShell Remote Session
-Enter-PSSession -ConnectionUri "https://nanoserver.eastus2.cloudapp.azure.com:5986/WSMAN" -Credential Navigator
-
-
-#############################
-
-
-# Get PowerShell Version
-$PSVersionTable; 
-Get-PSRepository
-Get-PackageProvider -ListAvailable
-
-
-##Nuget Provider Broken
-#https://github.com/OneGet/NanoServerPackage/issues/4
-
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force 
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted 
- 
-
-#Install all available updates 
-$sess = New-CimInstance -Namespace root/Microsoft/Windows/WindowsUpdate -ClassName MSFT_WUOperationsSession  
-$scanResults = Invoke-CimMethod -InputObject $sess -MethodName ApplyApplicableUpdates  
-Restart-Computer
-
-## THIS IS BROKEN --- 
-Install-PackageProvider NanoServerPackage
-Import-PackageProvider NanoServerPackage
-Install-NanoServerPackage -Name Microsoft-NanoServer-Storage-Package
-Install-NanoServerPackage -Name Microsoft-NanoServer-IIS-Package
-
-
-#############################
-
-#Testing
-
-# Deploy the Template to the Resource Group
-New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName `
-                                   -TemplateFile 'templates\WindowsNanoServerVM.json' `
-                                   -TemplateParameterFile 'templates\WindowsNanoServerVM.parameters.json' `
-                                   -Verbose
-
-
-https://alexandrebrisebois.wordpress.com/2016/02/18/deploying-nano-server-to-microsoft-azure/
-https://blogs.technet.microsoft.com/nanoserver/2016/05/27/nano-server-tp5-iaas-image-in-azure-updated
-
-
 $i = 1
 For ($i=1; $i -lt 2; $i++) {
   $VitualMachine = @{
@@ -87,3 +27,4 @@ For ($i=1; $i -lt 2; $i++) {
     . .\..\base\build-win-nano.ps1 @VitualMachine;
 
 }
+
