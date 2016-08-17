18090723fbf1d5bce3d595f4988182bf539c72c2
4c74356b41 <spam@4c74356b41.com>
2016-08-17 18:48:18 +0300

    fixed iisreset, fixed webappwebsite
    Works manually, doesn't work through dsc extension.

--- win-node/dsc/setup-iis-web.ps1
+++ win-node/dsc/setup-iis-web.ps1
@@ -1,96 +1,110 @@
-Configuration WebSite
-{ 
-    param ( 
-        [string[]]$NodeName = 'localhost'
-    ) 
+Configuration Payload
+{
 
-    Import-DscResource -Module xWebAdministration     
-    Import-DSCResource -Module xPSDesiredStateConfiguration 
-    Import-DSCResource -Module PSDesiredStateConfiguration
-    Import-DSCResource -Module xNetworking 
+Param ( [string] $nodeName )
 
-    Node $NodeName { 
+Import-DscResource -ModuleName PSDesiredStateConfiguration
+Import-DscResource -ModuleName xPSDesiredStateConfiguration
+Import-DscResource -ModuleName xWebAdministration
+Import-DscResource -ModuleName xNetworking
 
-	  WindowsFeature IIS 
-	  { 
-	        Name = "Web-Server"
-            Ensure = "Present"
-      }
+Node $nodeName
+  {
 
-      WindowsFeature IISManagement  
-      {  
-            Ensure          = "Present"
-            Name            = "Web-Mgmt-Console"
-            DependsOn       = "[WindowsFeature]IIS"
-      } 
-
-
-      xWebsite DefaultSite   
-      {  
-            Ensure          = "Present"
-            Name            = "Default Web Site"
-            State           = "Stopped"
-            PhysicalPath    = "C:\inetpub\wwwroot" 
-            DependsOn       = "[WindowsFeature]IIS"
-      } 
-
-
-      xRemoteFile DotNetCoreWindowsHosting
-      {  
-            Uri             = "https://go.microsoft.com/fwlink/?LinkId=817246"
-            DestinationPath = "C:\setup\DotNetCore.1.0.0-WindowsHosting.exe"
-      } 
-
-
-      xRemoteFile DotNetCoreSDK
-      {  
-            Uri             = "https://go.microsoft.com/fwlink/?LinkID=809122"
-            DestinationPath = "C:\setup\DotNetCore.1.0.0-SDK.Preview2-x64.exe"
-      } 
-
-
-      xRemoteFile AppVeyorDeploymentAgent
-      {  
-            Uri             = "https://www.appveyor.com/downloads/deployment-agent/latest/AppveyorDeploymentAgent.msi"
-            DestinationPath = "C:\setup\AppveyorDeploymentAgent.v3.12.0.msi"
-      } 
-
-
-
-      xRemoteFile TestWebSite
-      {  
-            Uri             = "https://github.com/stuartshay/AzureRM/raw/master/assets/webapp.zip"
-            DestinationPath = "C:\website\webapp.zip"
-      } 
-
-      Archive UnpackWebsite 
-      {
-            Ensure          = "Present" 
-            Path            = "C:\website\webapp.zip"
-            Destination     = "C:\website\"
-      }
-
-
-      xWebAppPool WebAppAppPool   
-      {  
-            Ensure          = "Present"  
-            Name            = "web-app" 
-            State           = "Started"
-            managedRuntimeVersion = ""
-      } 
-
-
-      xFirewall Http8080
-      {
-            Name         = "Htpp-8080"
-            DisplayName  = "Htpp-8080"
-            Ensure       = "Present"
-            Action       = "Allow"
-            Direction    = "Inbound"
-            LocalPort    = ("8080")
-            Protocol     = "TCP"
-      } 
-
+    LocalConfigurationManager 
+    { 
+        # This is false by default
+        RebootNodeIfNeeded = $true
     } 
 
+        WindowsFeature WebServerRole
+        {
+            Name = "Web-Server"
+            Ensure = "Present"
+        }
+
+        WindowsFeature WebManagementConsole
+        {
+            Name = "Web-Mgmt-Console"
+            Ensure = "Present"
+        }
+
+        xRemoteFile Payload
+        {
+            Uri             = "https://github.com/stuartshay/AzureRM/raw/master/assets/webapp.zip" 
+            DestinationPath = "C:\Setup\website.zip" 
+        }
+
+        xRemoteFile InstallDotNetCoreWindowsHosting
+        {
+            Uri             = "https://go.microsoft.com/fwlink/?LinkId=817246" 
+            DestinationPath = "C:\Setup\InstallDotNetCoreWindowsHosting.exe" 
+        }
+
+        xRemoteFile AppVeyorDeploymentAgent
+        {  
+             Uri             = "https://www.appveyor.com/downloads/deployment-agent/latest/AppveyorDeploymentAgent.msi"
+             DestinationPath = "C:\Setup\AppveyorDeploymentAgent.v3.12.0.msi"
+        } 
+
+        Archive WebAppExtract
+        {              
+             Path = "C:\Setup\website.zip"
+             Destination = "C:\inetpub\"
+             DependsOn = "[xRemoteFile]Payload"            
+        }
+
+        Package InstallDotNetCoreWindowsHosting
+        {
+               Ensure = "Present"
+               Path = "C:\Setup\InstallDotNetCoreWindowsHosting.exe"
+               Arguments = "/q /norestart"
+               Name = "DotNetCore"
+               ProductId = "4ADC4F4A-2D55-442A-8655-FBF619F94A69"
+               DependsOn = "[xRemoteFile]InstallDotNetCoreWindowsHosting"
+        }
+
+	xWebAppPool WebAppAppPool   
+        {  
+                Ensure          = "Present"  
+                Name            = "web-app" 
+                State           = "Started"
+                managedRuntimeVersion = ""
+                DependsOn       = "[WindowsFeature]WebServerRole"
+        }  
+
+	xWebsite WebAppWebSite   
+        {  
+                Ensure          = "Present"  
+                Name            = "web-app" 
+                State           = "Started"
+                PhysicalPath    = "C:\inetpub\webapp\wwwroot"
+                ApplicationPool = "web-app"
+                BindingInfo = MSFT_xWebBindingInformation
+                        {
+                            Port = '8080'
+                            IPAddress = '*'
+                            Protocol = 'HTTP'
+                        }
+                DependsOn = "[xWebAppPool]WebAppAppPool"
+        }
+
+        Script iisReset
+        {
+            TestScript =  { $false }
+            SetScript =  { iisreset }
+            GetScript = {@{Result = 'Will perform "iisreset" if invoked'}}
+            DependsOn =  @('[xWebsite]WebAppWebSite', '[xWebAppPool]WebAppAppPool')
+
+        }
+
+        xWebsite DefaultSite   
+        {  
+                Ensure          = "Present"
+                Name            = "Default Web Site"
+                State           = "Stopped"
+                PhysicalPath    = "C:\inetpub\wwwroot" 
+                DependsOn       = "[Script]iisReset"
+        }
+    }
 }
