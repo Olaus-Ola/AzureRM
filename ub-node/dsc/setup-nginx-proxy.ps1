d0e232b0f039cbcd4a6339aaedd93433733f4ffb
Stuart Shay <sshay@yahoo.com>
2016-08-25 14:59:41 -0400

    Fixed Nginx Script

--- ub-node/dsc/setup-nginx-proxy.ps1
+++ ub-node/dsc/setup-nginx-proxy.ps1
@@ -25,7 +25,7 @@ Import-DscResource -Module nx
         
         nxFile ProxyConf 
         {
-            SourcePath = "https://" + $StorageAccountName + ".blob.core.windows.net/config/nginx.conf"
+            SourcePath = "https://" + $StorageAccountName + ".blob.core.windows.net/nginx/nginx.conf"
             DestinationPath = '/etc/nginx/sites-available/proxy'
             DependsOn = '[nxPackage]Nginx'
         }  
