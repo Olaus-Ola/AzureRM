5f10669c27f5f9dfc6592252191045aa438b4931
Глеб Боушев < >
2016-09-17 12:41:09 +0300

    management studio

--- win-node/dsc/setup-sql-prerequisite.ps1
+++ win-node/dsc/setup-sql-prerequisite.ps1
@@ -68,5 +68,24 @@ Configuration Payload
                 DependsOn       = "[File]SetupDir"
             }
         }
+
+        if ($managementstudio) {
+            xRemoteFile ManagementStudioPackage
+            {  
+                Uri             = "http://go.microsoft.com/fwlink/?LinkID=824938"
+                DestinationPath = 'c:\Setup\SSMS-Setup-ENU.exe'
+                DependsOn       = "[File]SetupDir"
+            }
+
+            Package ManagementStudio
+            {
+                Ensure = "Present"
+                Path = "C:\Setup\SSMS-Setup-ENU.exe"
+                Arguments = "/q /norestart"
+                Name = "ManagementStudio"
+                ProductId = "446B31DB-00FC-4EEF-8B13-7F5F8A38F026"
+                DependsOn = "[xRemoteFile]ManagementStudioPackage"
+            }
+        }
     }
 }
