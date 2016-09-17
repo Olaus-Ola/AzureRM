3c3b528bc2ce967f4d4146a60685a17fc8a1f864
Глеб Боушев < >
2016-09-17 12:54:53 +0300

    optimized a bit

--- win-node/dsc/setup-sql-prerequisite.ps1
+++ win-node/dsc/setup-sql-prerequisite.ps1
@@ -8,28 +8,14 @@ Configuration Payload
     )
 
     Import-DscResource -ModuleName PSDesiredStateConfiguration
-    Import-DscResource -ModuleName xPSDesiredStateConfiguration -ModuleVersion 3.13.0.0
-    Import-DscResource -ModuleName xStorage -ModuleVersion 2.6.0.0
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration
+    Import-DscResource -ModuleName xStorage
 
     Node $nodeName
     {
         LocalConfigurationManager
         {
             RebootNodeIfNeeded = $true
-        }
-
-        xWaitforDisk Disk2
-        {
-             DiskNumber = 2
-             RetryIntervalSec = 60
-             RetryCount = 60
-        }
-
-        xDisk DataVolume
-        {
-             DiskNumber = 2
-             DriveLetter = 'M'
-             AllocationUnitSize = 64kb
         }
 
         File SetupDir
@@ -61,17 +47,11 @@ Configuration Payload
                 DestinationPath = 'c:\Setup\en_sql_server_2016_enterprise_x64_dvd_8701793.iso'
                 DependsOn       = "[File]SetupDir"
             }
-
-            xRemoteFile SQLServerMangementPackage
-            {  
-                Uri             = "http://go.microsoft.com/fwlink/?LinkID=824938"
-                DestinationPath = 'c:\Setup\SSMS-Setup-ENU.exe'
-                DependsOn       = "[File]SetupDir"
-            }
         }
 
         if ($managementstudio) {
-            xRemoteFile ManagementStudioPackage
+
+            xRemoteFile SQLServerMangementPackage
             {  
                 Uri             = "http://go.microsoft.com/fwlink/?LinkID=824938"
                 DestinationPath = 'c:\Setup\SSMS-Setup-ENU.exe'
@@ -85,7 +65,7 @@ Configuration Payload
                 Arguments = "/q /norestart"
                 Name = "ManagementStudio"
                 ProductId = "446B31DB-00FC-4EEF-8B13-7F5F8A38F026"
-                DependsOn = "[xRemoteFile]ManagementStudioPackage"
+                DependsOn = "[xRemoteFile]SQLServerMangementPackage"
             }
         }
     }
